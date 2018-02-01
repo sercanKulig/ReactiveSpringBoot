664086459d58fe29e9f5515fa01f6eaabb888134
Ünsal Sercan Küliğ <sercankulig@gmail.com>
2018-02-01 21:50:06 +0300

    new way to send request

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,10 +1,10 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import 'rxjs/Rx';
 
-import { RecipeService } from '../recipes/recipe.service';
-import { Recipe } from '../recipes/recipe.model';
-import { AuthService } from '../auth/auth.service';
-import {HttpClient, HttpHeaders, HttpParams} from "@angular/common/http";
+import {RecipeService} from '../recipes/recipe.service';
+import {Recipe} from '../recipes/recipe.model';
+import {AuthService} from '../auth/auth.service';
+import {HttpClient, HttpHeaders, HttpParams, HttpRequest} from "@angular/common/http";
 
 @Injectable()
 export class DataStorageService {
@@ -16,14 +16,18 @@ export class DataStorageService {
   storeRecipes() {
     const token = this.authService.getToken();
 
-    return this.httpClient.put('https://ng-recipe-book-3adbb.firebaseio.com/recipes.json', this.recipeService.getRecipes(),
-      {
-        observe: 'body',
-        headers: new HttpHeaders()
-          .set('Authorization', 'Bearer token')
-          .append('Authorization2','Bearer token 2'),
-        params: new HttpParams().set('auth', token)
-      });
+    /*    return this.httpClient.put('https://ng-recipe-book-3adbb.firebaseio.com/recipes.json', this.recipeService.getRecipes(),
+          {
+            observe: 'body',
+            headers: new HttpHeaders()
+              .set('Authorization', 'Bearer token')
+              .append('Authorization2','Bearer token 2'),
+            params: new HttpParams().set('auth', token)
+          });*/
+    const req = new HttpRequest('PUT', 'https://ng-recipe-book-3adbb.firebaseio.com/recipes.json', this.recipeService.getRecipes()
+      , {reportProgress: true, params: new HttpParams().set('auth', token)});
+    //report progress return loaded and total, between this two determinate time of the progress of request
+    return this.httpClient.request(req);
   }
 
   getRecipes() {

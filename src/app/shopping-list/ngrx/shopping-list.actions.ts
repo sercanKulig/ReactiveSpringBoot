08c65c0fc20ba02634a8f37bdc1423953ebfae61
sercan.kulig <sercankulig@gmail.com>
2018-02-02 15:36:12 +0300

    dispatch update-delete

--- src/app/shopping-list/ngrx/shopping-list.actions.ts
+++ src/app/shopping-list/ngrx/shopping-list.actions.ts
@@ -8,4 +8,26 @@ export class AddIngredient implements Action {
   constructor(public payload: Ingredient) {}
 }
 
-export type ShoppingListActions = AddIngredient;
+export const ADD_INGREDIENTS = 'ADD_INGREDIENTS';
+export class AddIngredients implements Action {
+  readonly  type = ADD_INGREDIENTS;
+  constructor(public payload: Ingredient[]) {}
+}
+
+export const UPDATE_INGREDIENT = 'UPDATE_INGREDIENT';
+export class UpdateIngredient implements Action {
+  readonly  type = UPDATE_INGREDIENT;
+  constructor(public payload: {index: number, ingredient: Ingredient}) {}
+}
+
+export const DELETE_INGREDIENT = 'DELETE_INGREDIENT';
+export class DeleteIngredient implements Action {
+  readonly  type = DELETE_INGREDIENT;
+  constructor(public payload: number) {}
+}
+
+export type ShoppingListActions =
+  AddIngredient |
+  AddIngredients |
+  UpdateIngredient |
+  DeleteIngredient;

80169b66ec98ccd0e9370d3518d55dc56eba340b
Ünsal Sercan Küliğ <sercankulig@gmail.com>
2018-02-03 18:31:14 +0300

    authState

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,15 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 import { DataStorageService } from '../../shared/data-storage.service';
 import { AuthService } from '../../auth/auth.service';
+import {Store} from "@ngrx/store";
+import * as fromApp from '../../store/app.reducers'
+import * as fromAuth from '../../auth/ngrx/auth.reducers'
+import {Observable} from "rxjs/Observable";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
+ authState: Observable<fromAuth.State>;
+
   constructor(private dataStorageService: DataStorageService,
-              private authService: AuthService) {
+              private authService: AuthService,
+              private store: Store<fromApp.AppState>) {
+  }
+
+  ngOnInit(): void {
+    this.authState = this.store.select('auth');
   }
 
   onSaveData() {
